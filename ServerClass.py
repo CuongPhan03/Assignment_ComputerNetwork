f50d42232c35b2e87939f91b67ca647c26c468e8
lptphuc <lamphamtrongphuc@gmail.com>
2023-10-31 20:05:24 +0700

    edit ServerClass

--- ServerClass.py
+++ ServerClass.py
@@ -2,20 +2,21 @@ from threading import Thread
 import socket
 import json
 
+
 class Server:
     IP = socket.gethostbyname(socket.gethostname())
     PORT = 5000
     FORMAT = "utf-8"
     SIZE = 1024
-    listFile = {"datas":[]}
+    listFile = {"datas": []}
     serverSocket = None
     listSocket = []
     jsonPeerDatas = []
     allThreads = []
     endAllThread = False
-    
+
     def startServer(self):
-        binder = Thread(target = self.listenMessage)
+        binder = Thread(target=self.listenMessage)
         self.allThreads.append(binder)
         binder.start()
 
@@ -35,8 +36,7 @@ class Server:
                     receiveData = conn.recv(self.SIZE).decode(self.FORMAT)
                     jsonData = json.loads(receiveData)
                     if (jsonData["action"] == "register"):
-                        jsonData["connection"] = conn
-                        self.jsonPeerDatas.append(jsonData)
+                        self.jsonPeerDatas.append(receiveData)
                     elif (jsonData["action"] == "publish"):
                         self.listFile["datas"].append(jsonData["fname"])
                     elif (jsonData["action"] == "reqListFile"):
@@ -47,26 +47,22 @@ class Server:
                     continue
 
     def sendListPeer(self, connection, fname):
-        datas = {"datas":[]}
+        datas = {"datas": []}
         for jsonPeerData in self.jsonPeerDatas:
             if (fname in jsonPeerData):
                 jsonData = json.loads(jsonPeerData)
-                data = {"name": jsonData["name"], "IP": jsonData["IP"], "port": jsonData["port"]}
+                data = {
+                    "name": jsonData["name"], "IP": jsonData["IP"], "port": jsonData["port"]}
                 datas["datas"].append(data)
         sendDatas = json.dumps(datas)
         connection.send(sendDatas.encode(self.FORMAT))
 
-    def getListPeer(self):
-        for peer in self.listFile:
-            pass
-
-    def getPeerInfo(self, connection):
-        pass
-
     def sendListFile(self, connection):
         # code
-        pass
-    
+        file_list = json.dumps(self.listFile)
+        connection.send(file_list.encode(self.FORMAT))
+    pass
+
     def endSystem(self):
         print("End system call")
         for socket in self.listSocket:
@@ -75,5 +71,3 @@ class Server:
         for thread in self.allThreads:
             del thread
         self.endAllThread = True
-    
-    
